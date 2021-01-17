ecbd0aad12c8a33b85c5b48d05812959e1b11616
Wallop Opasakhun <champbassist159@gmail.com>
2021-01-18 02:34:46 +0700

    fix css

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,8 @@
+import 'react-toastify/dist/ReactToastify.css';
 import 'adminbsb-materialdesign/plugins/bootstrap/css/bootstrap.css';
 import 'adminbsb-materialdesign/plugins/node-waves/waves.css';
 import 'adminbsb-materialdesign/plugins/animate-css/animate.css';
 import 'adminbsb-materialdesign/css/style.css';
-import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import '../styles/globals.css'
 
